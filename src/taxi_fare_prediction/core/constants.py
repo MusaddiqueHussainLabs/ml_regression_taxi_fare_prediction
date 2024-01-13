490d6b37eac62cc4c397d8989354307bb787b235
MusaddiqueHussainLabs <musaddiquehussainlabs@gmail.com>
2024-01-14 00:37:57 +0530

    restructured folders

--- src/taxi_fare_prediction/core/constants.py
+++ src/taxi_fare_prediction/core/constants.py
@@ -4,8 +4,8 @@ from pydantic_settings import BaseSettings
 class AppConstants(BaseSettings):
 
     CONFIG_FILE_PATH: Path =  Path("src/taxi_fare_prediction/core/config.yaml")
-    PARAMS_FILE_PATH: Path =  Path("params.yaml")
-    SCHEMA_FILE_PATH: Path =  Path("schema.yaml")
+    PARAMS_FILE_PATH: Path =  Path("src/taxi_fare_prediction/core/params.yaml")
+    SCHEMA_FILE_PATH: Path =  Path("src/taxi_fare_prediction/schemas/schema.yaml")
     STATUS_FILE_PATH: Path =  Path("data/interim/status.txt")
 
 constants =    AppConstants()
